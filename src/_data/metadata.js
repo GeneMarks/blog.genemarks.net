1ebb801a2d1de37cbe38255828db9aa80e44af62
Gene Marks <genemarks74@gmail.com>
2026-02-28 10:32:55 -0500

    Implement global metadata file with site details, add description meta tag to header

---
+++ src/_data/metadata.js
@@ -0,0 +1,7 @@
+export default {
+    url: "https://blog.genemarks.net",
+    siteName: "Gene's blog",
+    siteDesc: "Anything that comes to mind",
+    authorName: "Gene Marks",
+    authorEmail: "genemarks74@gmail.com",
+}
